ae04d23ddd3682c6d784a913fd862b43c24f1085
Md Sujon Mia <sujon258549@gmail.com>
2024-11-15 23:06:05 +0600

    modified app.js and app.ts in app

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,7 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.json());
+app.use(express_1.default.text());
 app.get('/', (req, res) => {
     res.send('Hello World Sujon mia sujon!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "This is successfuly insert data"
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
-import express from "express"
+
+
+import express,{Request, Response} from "express"
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World Sujon mia sujon!')
+app.use(express.json())
+app.use(express.text())
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World Sujon mia sujon!')
 })
 
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.json({
+        message:"This is successfuly insert data"
+    })
+})
 
 export default app;
